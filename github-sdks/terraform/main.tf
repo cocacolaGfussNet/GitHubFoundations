a06cde4130c276876abd4b6a5cdd90ffc84e6483
cocacolaGfussNet <161371973+cocacolaGfussNet@users.noreply.github.com>
2024-02-28 21:58:14 +0000

    github TF example

---
+++ github-sdks/terraform/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = "6.0.0"
+    }
+  }
+}
+
+provider "github" {
+  # Configuration options
+}
+
+resource "github_branch" "development" {
+    repository = "GitHubFoundations"
+    branch = "sdks"
+  
+}
